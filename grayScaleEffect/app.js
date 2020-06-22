66956995a6e152eeb59eb7009bf7ef67985eb70e
Aryan Bhandari <aryancodename47@gmail.com>
2020-06-22 21:37:27 +0530

    Give your image grayscale effect.

--- grayScaleEffect/app.js
+++ grayScaleEffect/app.js
@@ -18,3 +18,23 @@ function makeGray() {
 
     image.drawTo(imgcanvas);
 }
+
+//Downlaod button 
+function download() {
+
+    //for edge 
+    if (window.navigator.msSaveBlob) {
+        window.navigator.msSaveBlob(imgcanvas.msToBlob(), "grayscale-Image.pnj")
+    } else {
+
+        //for other browser
+        const a = document.createElement('a');
+
+        document.body.appendChild(a);
+        a.href = imgcanvas.toDataURL();
+        a.download = "grayscale-Image.png";
+        a.click();
+        document.body.removeChild(a);
+    }
+
+}
